c3c21c39e4591f89ec96ba61dc5f7bc7e5a78edb
罗涛 <luotovo 1569675241@qq.com>
2025-09-08 23:55:05 +0800

    订单状态定时处理 业务功能开发完成

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -6,6 +6,9 @@ import com.sky.entity.Orders;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Mapper
 public interface OrderMapper {
     /*
@@ -39,6 +42,15 @@ public interface OrderMapper {
     * 根据id查询订单
      */
     Orders getById(Long orderId);
+    /*
+    * 根据状态统计订单数量
+     */
 
     Integer countStatus(Integer status);
+
+    /*
+    根据状态和下单时间查询订单·
+     */
+    @Select("select * from orders where status = #{status} and order_time < #{orderTime}")
+    List<Orders> getByStatusAndOrderTime(Integer status, LocalDateTime orderTime);
 }
